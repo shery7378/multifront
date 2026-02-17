0dc9fcb07ce830a6853d7d1d01a7b825aa6bf98a
shery7378 <shehryar@quadtrum.com>
2026-02-17 22:30:55 +0500

    fixes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
-//postcss.config.mjs
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
+//postcss.config.mjs
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
 export default config;
+
